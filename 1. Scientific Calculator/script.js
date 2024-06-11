7fbb52c1150e007f8576e13f6026cb6de32dfad8
AbdulMunnam07 <createrking40@gmail.com>
2024-06-11 15:24:12 +0500

    Check

---
+++ 1. Scientific Calculator/script.js
@@ -0,0 +1,20 @@
+let click = ""
+let present_num = ""
+
+// console.log(res[0].firstElementChild.innerHTML = "current_num");
+
+
+
+// function result_calculation(){
+//     let res = document.getElementsByClassName("result");
+//     res[0].firstElementChild.innerHTML = current_num 
+// }
+
+function button_manupulation() {
+    for (let i = 0; i < btn1.length; i++) {
+        
+    }
+    let current = document.getElementsByClassName("btn1");
+    console.log(current[0].firstElementChild.innerHTML = "7");
+    // let current_num = current[0].firstElementChild.innerHTML = "7"    
+}
